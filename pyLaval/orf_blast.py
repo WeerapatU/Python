ee05c256c598c989e5a321c54d77a5d47198c26e
joey0214 <joey0576@163.com>
2014-09-30 18:22:27 -0400

    20140930tmp

--- pyLaval/orf_blast.py
+++ pyLaval/orf_blast.py
@@ -2,7 +2,9 @@
 
 from Bio.Blast import NCBIWWW, NCBIXML
 from Bio import SeqIO,Seq
-
+from Bio.Blast.Applications import NcbiblastxCommandline
+import csv
+import sys
 
 tmp_dna = "ATGGCCCATCTCTCACTGAATCAGTACCAAATGCACTCACATCATTATGCACGGCACTTGCCTCAGCGGTCTATACCCTGTGCCATTTACCCA"
 tmp_aa = "MAHLSLNQYQMHSHHYARHLPQRSIPCAIYP"
@@ -20,4 +22,16 @@ for record in blast_records:
 			# print hsp.sbjct
 
 # print blast_records
+
+#usage: python $0 csv_file_with_orf_and_translation database_file
+in_csv_file = sys.argv[1]
+csv_file = open(in_file,"r")
+
+in_database_file = sys.argv[2]
+
+csv_content = csv.reader(csv_file, delimiter=",")
+
+for row in csv_content:
+	equery_seq = row[6]
+	
 
