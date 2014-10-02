55852556c6d2be631a8bc14da8b9c4bc6685184b
joey0214 <joey0576@163.com>
2014-10-02 16:02:31 -0400

    20141002

---
+++ pyLaval/filter20141002.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+import sys
+import csv
+
+in_file_name = sys.argv[1]
+out_file_name = "filtered_blast_result.csv"
+
+in_file = open(in_file_name,"r")
+
+new_row = ""
+
+def list2string(thelist):
+	tmp_string =""
+
+	for item in thelist:
+		tmp_string += item +","
+	return tmp_string
+
+for row in csv.reader(in_file,delimiter=","):
+	if len(row) == 10:
+		new_row += list2string(row) + "\n"
+
+output = open(out_file_name,"w")
+output.write(new_row)
+
+output.close()
+in_file.close()
