ca4bfa837be5a6218c0c12190055579e7a169af4
joey0214 <joey0576@163.com>
2014-11-24 11:15:45 -0500

    2014-11-24-get sra batrch

--- pyLaval/getSRAdata_batch.py
+++ pyLaval/getSRAdata_batch.py
@@ -19,13 +19,25 @@ def directGet(infile):
 def getFromSummary(infile):
 	lines = infile.readlines()[1:] 
 	for line in lines:
-		url =  (line.split(",")[14])[1:-1]
-		cmd_str = "wget " + url
+		url =  (line.split(",")[14])[1:-1] 
+		accept_str = line.split(",")[0][1:-1]+".sra"
+		cmd_str = "wget -r --accept " +accept_str +" --level 2  " + url
 		os.system(cmd_str)
 
+#get sra files from accession list file.
+def getByAccession(infile):
+	for line in infile:
+		accession = line[0:-1]
+		url =  "ftp://ftp-trace.ncbi.nih.gov"
+		###/sra/sra-instant/reads/ByRun/sra/{SRR|ERR|DRR}/<first 6 characters of accession>/<accession>/<accession>.sra
+		path = "/sra/sra-instant/reads/ByRun/sra/SRR/%s/%s/%s.sra" %(accession[0:6], accession, accession)
+		os.system("wget " +url+path)
+		
 ###main program
 #directGet(infilename)
 
-getFromSummary(infile)
+#getFromSummary(infile)
+
+getByAccession(infile)
 
 infile.close()
