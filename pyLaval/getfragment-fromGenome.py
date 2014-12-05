00f2777e7f7b1961e1809f410d866697ede90256
joey0214 <joey0576@163.com>
2014-12-05 01:57:19 -0500

    2014-12-05updates

--- pyLaval/getfragment-fromGenome.py
+++ pyLaval/getfragment-fromGenome.py
@@ -3,6 +3,8 @@
 import sys
 import csv
 from Bio import SeqIO
+from Bio.Alphabet import generic_dna
+from Bio.Seq import Seq
 
 csv_infilename = sys.argv[1]
 genome_infilename = sys.argv[2]
@@ -20,25 +22,45 @@ def read_Genome(genome_infilename):
 	genome_infile.close()
 	return seq_dict
 
+##get rever complement sequence
+def seq_rever_complement(sequence_string):
+	dna_seq = Seq(sequence_string, generic_dna)
+	reverse_seq = dna_seq.reverse_complement()
+	return str(reverse_seq)
+
+##write out
 def write_out(filename, filecontent):
 	output = open(filename, "w")
 	output.write(filecontent)
 	output.close()
 
+##get genome dictionary
 genome_dict = read_Genome(genome_infilename)
 
+##sequence pool
 filecontent = ""
+
 for line in csv.reader(csv_infile, delimiter=","):
 	orfID = line[0]
 	position = line[2]
 	chroms_name = position.split(":")[0]
 	start_number, end_number = position.split(":")[1].split("-")
-	print chroms_name
-	dnaSeq = str(genome_dict[chroms_name])[(int(start_number)-1):int(end_number)]
+	if chroms_name in genome_dict.keys():
+		#this orf is on forward sequence
+		if start_number < end_number:
+			dnaSeq = str(genome_dict[chroms_name])[(int(start_number)-1):int(end_number)]
+			filecontent += ">" + orfID +"-F"+"\n"+dnaSeq+"\n"
+		#this orf is on the reverse complement sequence
+		else:
+			tmp_seq = str(genome_dict[chroms_name])[(int(end_number)-1):int(start_number )]
+			dnaSeq = seq_rever_complement(tmp_seq)
+			filecontent += ">" + orfID +"-R"+"\n"+dnaSeq+"\n"
 
-	filecontent += ">" + orfID +"\n"+dnaSeq+"\n"
+	else:
+		print chroms_name + "	is not found in genome file.Have a check."
+	
 
-write_out("fasat_orf.fasta", filecontent)
+write_out("fasta_orf.fasta", filecontent)
 
 
 csv_infile.close()
---
+++ pyLaval/processFASTQ.py
@@ -0,0 +1,32 @@
+#!usr/bin/python
+
+import sys
+from Bio import SeqIO
+
+def trim_adaptors(records, adaptor):
+
+    len_adaptor = len(adaptor) #cache this for later
+    for record in records:
+        index = record.seq.find(adaptor)
+        if index == -1:
+            #adaptor not found, so won't trim
+            yield record
+        else:
+            #trim off the adaptor
+            yield record[index+len_adaptor:]
+
+def trim_amubiguousN(records):
+    for record in records:
+        index = record.seq.find("N")
+        if index == -1:
+            yield record
+        else:
+            yield record[index:]
+
+
+infileneme = sys.argv[1]
+original_reads = SeqIO.parse(infileneme, "fastq")
+##trimmed_reads = trim_adaptors(original_reads, "GATGACGGTGT")
+trimed_reads = trim_amubiguousN(original_reads)
+count = SeqIO.write(trimmed_reads, "trimmed.fastq", "fastq") 
+print("Saved %i reads" % count)
